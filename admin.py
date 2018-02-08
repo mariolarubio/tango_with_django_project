990d5ef8c0742316ac64ecfc712deb8f8d1624ab
mariolarubio <35569797+mariolarubio@users.noreply.github.com>
2018-02-08 14:18:31 +0000

    Adding all project files to remote repository

---
+++ admin.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import admin
+
+from rango.models import Category, Page
+
+
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
+
+
+
+admin.site.register(Category)
+
+admin.site.register(Page, PageAdmin)
---
+++ views.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+def index(request):
+    context_dict = {'boldmessage':"Crunchy, creamy, cookie, candy, cupcake!"}
+    return render(request, 'rango/index.html', context=context_dict)
+
+def about(request):
+    context_dict = {'MEDIA_URL':"/media/"}
+    return render(request, 'rango/about.html', context=context_dict)
